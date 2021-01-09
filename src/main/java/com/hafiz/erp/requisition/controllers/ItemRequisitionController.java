f2ff373171ef4498937ff0df8cc13c0cd3ecf02f
Hafiz Ahmed <rhafizubd@gmail.com>
2021-01-09 22:46:01 +0600

    [wip] Overriding update method in ItemRequisitionController is in progress

--- src/main/java/com/hafiz/erp/requisition/controllers/ItemRequisitionController.java
+++ src/main/java/com/hafiz/erp/requisition/controllers/ItemRequisitionController.java
@@ -7,9 +7,12 @@ import com.hafiz.erp.requisition.entities.ItemRequisition;
 import com.hafiz.erp.requisition.services.ItemRequisitionService;
 import com.hafiz.erp.requisition.services.ItemRequisitionWrapperService;
 import com.hafiz.erp.requisition.utility.ItemRequisitionConversionUtility;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(EndPoint.ITEM_REQUISITION)
@@ -24,8 +27,13 @@ public class ItemRequisitionController extends CrudController<ItemRequisition, I
     }
 
     @Override
-    public ItemRequisitionDTO create(@RequestBody ItemRequisitionDTO d) {
-        return wrapperService.createRequisitionWithDetails(d);
+    public ItemRequisitionDTO create(@RequestBody ItemRequisitionDTO reqBody) {
+        return wrapperService.createRequisitionWithDetails(reqBody);
+    }
+
+    @Override
+    public ItemRequisitionDTO update(@RequestBody ItemRequisitionDTO reqBody, @PathVariable UUID id) {
+        return wrapperService.updateRequisitionWithDetails(reqBody, id);
     }
 
 }
--- src/main/java/com/hafiz/erp/requisition/services/ItemRequisitionWrapperService.java
+++ src/main/java/com/hafiz/erp/requisition/services/ItemRequisitionWrapperService.java
@@ -1,14 +1,15 @@
 package com.hafiz.erp.requisition.services;
 
 import com.hafiz.erp.requisition.dataclass.ItemRequisitionDTO;
+import com.hafiz.erp.requisition.entities.ItemRequisition;
 import com.hafiz.erp.requisition.utility.ItemRequisitionConversionUtility;
 import com.hafiz.erp.requisition.utility.ItemRequisitionDetailConversionUtility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,17 +22,30 @@ public class ItemRequisitionWrapperService {
 
 
     @Transactional
-    public ItemRequisitionDTO createRequisitionWithDetails(ItemRequisitionDTO d){
-        Optional<ItemRequisitionDTO> dto = conversionUtility.getDto(
-            Optional.ofNullable(service.create(
-                conversionUtility.buildEntity(Optional.of(d)))));
-        dto.ifPresent(itemRequisitionResponseDTO -> itemRequisitionResponseDTO.setDetails(
-                new HashSet<>(detailConversionUtility.getDtoList(
-                        detailService.createAll(
-                                d.getDetails().stream()
-                                        .peek(e -> e.setReqId(dto.get().getId()))
-                                        .map(e -> detailConversionUtility.buildEntity(Optional.of(e)))
-                                        .map(Optional::get).collect(Collectors.toList()))))));
-        return dto.orElse(null);
+    public ItemRequisitionDTO createRequisitionWithDetails(ItemRequisitionDTO reqBody){
+        //Create requisition
+        Optional<ItemRequisitionDTO> resBody = conversionUtility.getDto(
+            Optional.ofNullable(service.create(conversionUtility.buildEntityForCreate(Optional.of(reqBody)))));
+        //Create requisition details
+        resBody.ifPresent(itemRequisitionDTO -> createDetails(reqBody, itemRequisitionDTO.getId()));
+
+        return resBody.orElse(null);
+    }
+
+    private void createDetails(ItemRequisitionDTO d, UUID reqId) {
+        detailConversionUtility.getDtoList(
+            detailService.createAll(
+                d.getDetails().stream()
+                    .peek(e -> e.setReqId(reqId))
+                    .map(e -> detailConversionUtility.buildEntityForCreate(Optional.of(e)))
+                    .map(Optional::get).collect(Collectors.toList())));
+    }
+
+    public ItemRequisitionDTO updateRequisitionWithDetails(ItemRequisitionDTO reqBody, UUID reqId) {
+        Optional<ItemRequisition> entity = service.getById(reqId);
+        if(!entity.isPresent()) throw new RuntimeException("Entity not found with id: " + reqId);
+        Optional<ItemRequisitionDTO> resBody = conversionUtility.getDto(
+            Optional.ofNullable(service.update(conversionUtility.buildEntityForUpdate(Optional.of(reqBody), entity.get()))));
+        return resBody.orElse(null);
     }
 }
